8ff08642b7cb3ed717864f375eb6d7c32d48284d
Zefir Nikolov <113708651+zefirnikolov@users.noreply.github.com>
2023-04-25 14:19:12 +0300

    success

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.
+          Learn React. Successfully deployed
         </a>
       </header>
     </div>
